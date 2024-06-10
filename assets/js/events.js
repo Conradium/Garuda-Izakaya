1a03bb38d7617a4b8a5324071cdf552e399927b3
Conradium <40384712+Conradium@users.noreply.github.com>
2024-06-11 00:17:16 +0900

    Final Revision of Garuda Izakaya
    1. Fixed item contents in events
    2. Fixed index.html gallery overflowing
    3. Fixed missing SNS logo in footer
    4. Fixed Navigation bar selected category not lighting up correctly
    5. Fixed logo and title page not clicking the link for home page
    6. Fixed icons
    7. Beautified HTML, JS, CSS codes to make it readable
    8. Improved readability in smaller devices.

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,119 +1,88 @@
-$(document).ready(function(){
-	
+// <!-- DEAR PROFESSOR, IF YOU ARE READING THIS, I AM SORRY, THIS CODE HAS A LOT OF TRASH CODES THAT DOESNT MAKE IT TO THE FINAL -->
+// <!-- DRAFT. FOR YOUR CONVENIENCE, ALL OF THE CODES HAVE BEEN BEAUTIFIED FOR EASY READING. ALSO HALFWAY OF DOING THIS, I JUST -->
+// <!-- REALIZED THAT IMG SOURCES THAT ARE PUT INSIDE CSS FILES CANNOT BE ON TOP OF THE DIRECTORY OF THE CSS FILE. -->
+// <!-- I WILL USE A BETTER HABIT NEXT TIME. 22229515 BENEDICTUS SEBASTIAN AP "CONRADIUM"-->
+$(document).ready(function() {
     //reset the search filters
-
-    $('#reset_search').on('click',function(){
-        $('.combine').each(function(e){
+    $('#reset_search').on('click', function() {
+        $('.combine').each(function(e) {
             $(this).val('');
         });
         $('.event').show();
         $('.month').show();
-        
         $('.accordion_content').hide();
     });
-
     //combine the results into one selector
-
-    $('.combine').on('change',function(){
+    $('.combine').on('change', function() {
         //
         var month_option = $('#month').val();
         var sector_option = $('#sector').val();
         var vendor_option = $('#vendor').val();
         var theme_option = $('#theme').val();
-        
         $('.event').hide();
         $('.month').hide();
-        
-        // create an empty variable, so we can add parts to it
         var selector = "";
-        
-        // when checking a variable to see if it has any matching values or not, treat it as a boolean. 
-        // it will either be true or false - if it's true then the value will be added to the 'selector' variable
-        if (month_option){
-            selector += "[data-month*=" +  month_option + "]";
+        if (month_option) {
+            selector += "[data-month*=" + month_option + "]";
+        }
+        if (sector_option) {
+            selector += "[data-sector*=" + sector_option + "]";
         }
         // when checking a variable to see if it has any matching values or not, treat it as a boolean. 
         // it will either be true or false - if it's true then the value will be added to the 'selector' variable
-        if (sector_option){
-            selector += "[data-sector*=" +  sector_option + "]";
+        if (vendor_option) {
+            selector += "[data-vendor*=" + vendor_option + "]";
         }
-        // when checking a variable to see if it has any matching values or not, treat it as a boolean. 
-        // it will either be true or false - if it's true then the value will be added to the 'selector' variable
-        if (vendor_option){
-            selector += "[data-vendor*=" +  vendor_option + "]";
+        if (theme_option) {
+            selector += "[data-theme*=" + theme_option + "]";
         }
-        // when checking a variable to see if it has any matching values or not, treat it as a boolean. 
-        // it will either be true or false - if it's true then the value will be added to the 'selector' variable
-        if (theme_option){
-            selector += "[data-theme*=" +  theme_option + "]";
-        }
-        
-        
-        // when checking a variable to see if it has any matching values or not, treat it as a boolean. 
-        // it will either be true or false - if it's true then the code in the if statement will run
-        if(selector){
+        if (selector) {
             var matches = $(selector);
-            
-            //show and matched elements as needed
             matches.fadeIn();
-            //as the parent container is also hidden, show that too
             matches.parent().fadeIn();
-            
-            //use length to check the number of matched elements and if there aren't any then alert the user
-            if(!matches.length){
-                //alert('no matches');
+            if (!matches.length) {
                 $('#no_event').addClass('active');
-                
             }
-            
             //keep a track on the values and number found and log them to the console
-            console.log(selector + " length:" + $(selector).length);		
-        }else{
+            console.log(selector + " length:" + $(selector).length);
+        }
+        else {
             //if none of the above are found then show all
             $('.event').show();
             $('.month').show();
         }
-        
     });
-
-/* ====================
-=======================
-=======================
-==================== */
-
-//CLICK EVENT TO SHOW MODAL POP UP
-
-$('.showModal').on('click',function(){
-    var targetId = $(this).attr('href');
-    var eventLink = $(this).data("link-href");
-    $(targetId).addClass('active');
-    return false;
-});	
-
-// $('.closeModal').on('click',function(){
-//     $('.modal').removeClass('active');
-//     $('.event').show();
-//     $('.month').show();
-//     $('.combine').each(function(e){
-//             $(this).val('');
-//         });
-// });
-
-/* ====================
-=======================
-=======================
-==================== */
-
-
+    /* ====================
+    =======================
+    =======================
+    ==================== */
+    //CLICK EVENT TO SHOW MODAL POP UP
+    $('.showModal').on('click', function() {
+        var targetId = $(this).attr('href');
+        var eventLink = $(this).data("link-href");
+        $(targetId).addClass('active');
+        return false;
+    });
+    // $('.closeModal').on('click',function(){
+    //     $('.modal').removeClass('active');
+    //     $('.event').show();
+    //     $('.month').show();
+    //     $('.combine').each(function(e){
+    //             $(this).val('');
+    //         });
+    // }); DONT USE THIS, I REMOVED THE X
+    /* ====================
+    =======================
+    =======================
+    ==================== */
 });
-
 $('.modal').on('click', function(event) {
     if ($(event.target).is($(this)) || $(event.target).parents('.modalContent').length === 0) {
-      $(this).removeClass('active');
-      $('.event').show();
-      $('.month').show();
-      $('.combine').each(function(e){
-        $(this).val('');
-      });
+        $(this).removeClass('active');
+        $('.event').show();
+        $('.month').show();
+        $('.combine').each(function(e) {
+            $(this).val('');
+        });
     }
-  });
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,104 +1,93 @@
+// <!-- DEAR PROFESSOR, IF YOU ARE READING THIS, I AM SORRY, THIS CODE HAS A LOT OF TRASH CODES THAT DOESNT MAKE IT TO THE FINAL -->
+// <!-- DRAFT. FOR YOUR CONVENIENCE, ALL OF THE CODES HAVE BEEN BEAUTIFIED FOR EASY READING. ALSO HALFWAY OF DOING THIS, I JUST -->
+// <!-- REALIZED THAT IMG SOURCES THAT ARE PUT INSIDE CSS FILES CANNOT BE ON TOP OF THE DIRECTORY OF THE CSS FILE. -->
+// <!-- I WILL USE A BETTER HABIT NEXT TIME. 22229515 BENEDICTUS SEBASTIAN AP "CONRADIUM"-->
 /*=============== SHOW MENU ===============*/
 const headerToggle = document.getElementById('header-toggle'),
-      main = document.getElementById('main'),
-      navClose = document.getElementById('nav-close')
-
+    main = document.getElementById('main'),
+    navClose = document.getElementById('nav-close')
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(headerToggle){
-    headerToggle.addEventListener('click', () =>{
+if (headerToggle) {
+    headerToggle.addEventListener('click', () => {
         main.classList.add('show-menu')
     })
 }
-
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         main.classList.remove('show-menu')
     })
 }
-
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const main = document.getElementById('main')
     // When we click on each nav__link, we remove the show-menu class
     main.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
+function scrollHeader() {
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if (this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 let currentSectionIndex = 0
 const sectionIds = ['home', 'reviews', 'about']
 const sections = []
-
 sectionIds.forEach(id => {
-  const section = document.querySelector(`section[id="${id}"]`)
-  if (section) sections.push(section)
+    const section = document.querySelector(`section[id="${id}"]`)
+    if (section) sections.push(section)
 })
-
 window.addEventListener('wheel', (e) => {
-  if (e.deltaY > 0) {
-    currentSectionIndex++
-    if (currentSectionIndex >= sections.length) {
-      currentSectionIndex = sections.length - 1
+    if (e.deltaY > 0) {
+        currentSectionIndex++
+        if (currentSectionIndex >= sections.length) {
+            currentSectionIndex = sections.length - 1
+        }
     }
-  } else {
-    currentSectionIndex--
-    if (currentSectionIndex < 0) {
-      currentSectionIndex = 0
+    else {
+        currentSectionIndex--
+        if (currentSectionIndex < 0) {
+            currentSectionIndex = 0
+        }
     }
-  }
-
-  sections.forEach((section, index) => {
-    if (index === currentSectionIndex) {
-      section.classList.add('active')
-      window.scrollTo({
-        top: section.offsetTop,
-        behavior: 'smooth'
-      })
-    } else {
-      section.classList.remove('active')
-    }
-  })
+    sections.forEach((section, index) => {
+        if (index === currentSectionIndex) {
+            section.classList.add('active')
+            window.scrollTo({
+                top: section.offsetTop,
+                behavior: 'smooth'
+            })
+        }
+        else {
+            section.classList.remove('active')
+        }
+    })
 })
-
-
+/*=============== index Logo ANIMATION ===============*/
 const logoImage = document.querySelector('.logo_image');
 const conceptQuote = document.querySelector('.concept-quote');
-
 window.addEventListener('load', () => {
-  setTimeout(() => {
-    conceptQuote.classList.add('show');
-    logoImage.classList.add('pushed');
-  }, 500); // adjust the delay to your liking
+    setTimeout(() => {
+        conceptQuote.classList.add('show');
+        logoImage.classList.add('pushed');
+    }, 500); // adjust the delay to your liking
 });
-
-
-//Review Part
+/*=============== REVIEWS ===============*/
 $(".review-user").hover(function() {
-  var $this = $(this); 
-  var userId = $this.data('user-id'); 
-  var $userData = $this.next('.user-data'); 
-
-  $.get("/user/data/" + userId, function(userData) {
-    // write user data to DOM
-    $userData.find('.profile-username').text(userData.username);
-    $userData.find('.profile-reviews').text(userData.reviews + 'eviews');
-  }); 
-  
-  $userData.show();
+    var $this = $(this);
+    var userId = $this.data('user-id');
+    var $userData = $this.next('.user-data');
+    $.get("/user/data/" + userId, function(userData) {
+        $userData.find('.profile-username').text(userData.username);
+        $userData.find('.profile-reviews').text(userData.reviews + 'eviews');
+    });
+    $userData.show();
 }, function() {
-  $(this).next('.user-data').hide();
+    $(this).next('.user-data').hide();
 });
-
